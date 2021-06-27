59da7f4b9c00c6f9850f6f272960a8885c8c483e
Russ Smith <gumper@gumper.com>
2021-06-27 07:33:26 -0700

    Don't install golang if it already exists

--- chef/cookbooks/workstation/recipes/golang.rb
+++ chef/cookbooks/workstation/recipes/golang.rb
@@ -3,21 +3,23 @@
 # Recipe:: golang
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
-remote_file "#{Chef::Config[:file_cache_path]}/go1.16.5.linux-amd64.tar.gz" do
+gotarball = "#{Chef::Config[:file_cache_path]}/go1.16.5.linux-amd64.tar.gz"
+remote_file "#{gotarball}" do
   source 'https://golang.org/dl/go1.16.5.linux-amd64.tar.gz'
 end
 
 go_vers = "go#{node['languages']['go']['version']}"
-log "go version #{go_vers}"
 directory '/usr/local/go' do
   recursive true
   action :delete
+  not_if gotarball.include? go_vers
 end
 
 archive_file "#{Chef::Config[:file_cache_path]}/go1.16.5.linux-amd64.tar.gz" do
   destination '/usr/local'
   overwrite true
   action :extract
+  not_if gotarball.include? go_vers
 end
 
 ruby_block 'Add go to PATH' do
