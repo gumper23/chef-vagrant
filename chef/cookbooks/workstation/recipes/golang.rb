7a64500f1027ac5e4f8bc17885f1c0d6377bc8ea
Russ Smith <gumper@gumper.com>
2021-06-27 09:30:12 -0700

    Rewrote regular expression; changed node key references

--- chef/cookbooks/workstation/recipes/golang.rb
+++ chef/cookbooks/workstation/recipes/golang.rb
@@ -3,12 +3,12 @@
 # Recipe:: golang
 #
 # Copyright:: 2021, Me, All Rights Reserved.
-gotarball = "go1.16.5.linux-amd64.tar.gz"
+gotarball = 'go1.16.5.linux-amd64.tar.gz'
 remote_file "#{Chef::Config[:file_cache_path]}/#{gotarball}" do
   source "https://golang.org/dl/#{gotarball}"
 end
 
-govers = node[:languages] && node[:languages][:go] && node[:languages][:go][:version] || '0.0.0'
+govers = node['languages'] && node['languages']['go'] && node['languages']['go']['version'] || '0.0.0'
 directory '/usr/local/go' do
   recursive true
   action :delete
@@ -25,7 +25,7 @@ end
 ruby_block 'Add go to PATH' do
   block do
     file = Chef::Util::FileEdit.new('/etc/bash.bashrc')
-    file.insert_line_if_no_match('export PATH="${PATH}:/usr/local/go/bin"', 'export PATH="${PATH}:/usr/local/go/bin"')
+    file.insert_line_if_no_match('export PATH="\$\{PATH}\:\/usr\/local\/go\/bin"', 'export PATH="${PATH}:/usr/local/go/bin"')
     file.write_file
   end
 end
