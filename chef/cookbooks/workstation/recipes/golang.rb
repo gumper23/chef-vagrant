7d424bd7b90039e200830032b0ed0a1152774808
Russ Smith <gumper@gumper.com>
2021-06-27 08:59:02 -0700

    Enhanced check for golang version

--- chef/cookbooks/workstation/recipes/golang.rb
+++ chef/cookbooks/workstation/recipes/golang.rb
@@ -8,18 +8,18 @@ remote_file "#{gotarball}" do
   source 'https://golang.org/dl/go1.16.5.linux-amd64.tar.gz'
 end
 
-go_vers = "go#{node['languages']['go']['version']}"
+govers = node[:languages] && node[:languages][:go] && node[:languages][:go][:version] || '0.0.0'
 directory '/usr/local/go' do
   recursive true
   action :delete
-  not_if { gotarball.include? go_vers }
+  not_if { gotarball.include? govers }
 end
 
 archive_file "#{Chef::Config[:file_cache_path]}/go1.16.5.linux-amd64.tar.gz" do
   destination '/usr/local'
   overwrite true
   action :extract
-  not_if { gotarball.include? go_vers }
+  not_if { gotarball.include? govers }
 end
 
 ruby_block 'Add go to PATH' do
