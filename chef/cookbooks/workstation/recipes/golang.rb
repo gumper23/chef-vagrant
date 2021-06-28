af12c6715eba89e937747f827cd552b9d448a770
Russ Smith <gumper@gumper.com>
2021-06-28 01:45:06 -0700

    Changed empty goversion test

--- chef/cookbooks/workstation/recipes/golang.rb
+++ chef/cookbooks/workstation/recipes/golang.rb
@@ -11,6 +11,7 @@ end
 # goversion = node['languages'] && node['languages']['go'] && node['languages']['go']['version'] || '0.0.0'
 gobin = '/usr/local/go/bin/go'
 goversion = %x[command -v #{gobin} && #{gobin} version | cut -d' ' -f 3]
+goversion = '0.0.0' if goversion.empty? 
 goversion = goversion.presence || '0.0.0'
 
 log "gotarball = [#{gotarball}]"
